a1ae0911a1dec8f34d4f2c24a2d4314567134aa1
suaemp <wiktoria.sladowska@gmail.com>
2021-11-13 14:30:06 +0100

    folders

---
+++ view/terminal.py
@@ -0,0 +1,8 @@
+import os
+
+def console_clear():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
